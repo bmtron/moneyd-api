f96eaa10ed790650f26d27c4d007eb7196a0ae2f
Brendan Meehan <brendan@brendanmeehan.com>
2025-12-13 22:04:21 -0500

    added transaction_type_lookup_code support, to go along with the OFX client refactor. Also, important todo in main.go regarding endpoint security

--- models/transaction.go
+++ models/transaction.go
@@ -6,11 +6,12 @@ import (
 
 
 type Transaction struct {
-	TransactionId		int			`json:"transaction_id"`
-	StatementId			int			`json:"statement_id"`
-	Description			string		`json:"description"`
-	Amount				int64		`json:"amount"`
-	TransactionDate		time.Time	`json:"transaction_date"`
-	DateAdded			time.Time	`json:"date_added"`
-	DateUpdated			time.Time	`json:"date_updated"`
+	TransactionId				int			`json:"transaction_id"`
+	StatementId					int			`json:"statement_id"`
+	TransactionTypeLookupCode 	int   		`json:"transaction_type_lookup_code"`
+	Description					string		`json:"description"`
+	Amount						int64		`json:"amount"`
+	TransactionDate				time.Time	`json:"transaction_date"`
+	DateAdded					time.Time	`json:"date_added"`
+	DateUpdated					time.Time	`json:"date_updated"`
 }
